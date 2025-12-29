dc51d50b7428dc402c13e0b93ce4ab6dc19774c0
IgorSilaire <igor.silaire@gmail.com>
2025-12-29 23:30:00 +0100

    add express server

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (_req, res) => {
+    res.send("Justify API running!");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+});
