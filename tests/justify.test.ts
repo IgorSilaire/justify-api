6e17cf34ef31b5d3fea99683eb21987be35d17a5
IgorSilaire <igor.silaire@gmail.com>
2026-01-04 15:52:11 +0100

    justify/test

--- tests/justify.test.ts
+++ tests/justify.test.ts
@@ -1,2 +1,26 @@
 import { justifyText } from "../src/justify";
 import {expect, jest, test} from "@jest/globals";
+
+function randomWords(count: number): string {
+    return Array.from({length: count})
+        .map(() => "word")
+        .join(" ");
+}
+
+test("text lines justified must be 80 characters long", () => {
+    
+    const text = randomWords(600);
+
+    const out = justifyText(text);
+
+    const lines = out.split("\n");
+
+    for (let i = 0; i < lines.length - 1; i++) {
+        expect(lines[i]?.length).toBe(80)
+    }
+    
+
+    const lastLine = lines[lines.length - 1];
+    expect(lastLine).toBeDefined();
+    expect(lastLine.length).toBeLessThan(80);
+})
