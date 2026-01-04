14839d8e53c0aba4c709af02684aa5821bdef303
IgorSilaire <igor.silaire@gmail.com>
2026-01-04 18:05:56 +0100

    fixing for /dist

---
+++ dist/rateLimit.d.ts
@@ -0,0 +1,4 @@
+export declare const DAILY_LIMIT = 80000;
+export declare function countWords(text: string): number;
+export declare function validQuota(token: string, text: string): boolean;
+//# sourceMappingURL=rateLimit.d.ts.map
